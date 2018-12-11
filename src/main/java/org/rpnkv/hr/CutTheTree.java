ea79208f8a781c3b9480954d24c3aa2250fd1d52
Sergei <theierus@gmail.com>
2018-12-11 23:02:05 +0300

    added maven before refactoring

--- src/main/java/org/rpnkv/hr/CutTheTree.java
+++ src/main/java/org/rpnkv/hr/CutTheTree.java
@@ -1,26 +1,34 @@
 package org.rpnkv.hr;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 class TreeCutter {
 
     private int[] weights;
+    private final List<List<Integer>> edges;
     private boolean inсidence[][];
     private int[][] subtreesWeights;
+    private int totalWeight = 0;
 
     public TreeCutter(List<Integer> data, List<List<Integer>> edges) {
         weights = new int[data.size()];
+        this.edges = edges;
         for (int i = 0; i < data.size(); i++) {
             weights[i] = data.get(i);
+            totalWeight += weights[i];
         }
 
         inсidence = new boolean[data.size()][edges.size()];
         for (int i = 0; i < edges.size(); i++) {
             List<Integer> edge = edges.get(i);
-            inсidence[edge.get(0)][i] = true;
-            inсidence[edge.get(1)][i] = true;
+            inсidence[edge.get(0) - 1][i] = true;
+            inсidence[edge.get(1) - 1][i] = true;
         }
 
         subtreesWeights = new int[edges.size()][];
@@ -36,55 +44,76 @@ class TreeCutter {
      */
 
     public int cutTheTree() {
-        int totalWeight = totalWeight();
+        cutTrees();
 
-
-
-        return 0;
+        return smallestDiff();
     }
 
-    private int totalWeight() {
-        return 0;
-    }
-
-    private int cutTrees() {
-        int total = weights[0];
+    private void cutTrees() {
         for (int edgeIndex = 0; edgeIndex < inсidence[0].length; edgeIndex++) {
             if (inсidence[0][edgeIndex]) {
                 int childIndex = getChildIndex(edgeIndex, 0);
-                total += sum(edgeIndex, childIndex);
+                sum(edgeIndex, childIndex);
+                int edgeSum = subtreesWeights[edgeIndex][0];
+                subtreesWeights[edgeIndex][1] = totalWeight - edgeSum;
             }
         }
-        return total;
     }
 
     private int getChildIndex(int edgeIndex, int parentIndex) {
-        for (int i = 0; i < weights.length; i++) {
+        /*for (int i = 0; i < weights.length; i++) {
             if (inсidence[i][edgeIndex] && i != parentIndex) {
                 return i;
             }
         }
         throw new IllegalArgumentException(String.format("Cannot find second node for edge %d with parent %d",
-                edgeIndex, parentIndex));
+                edgeIndex, parentIndex));*/
+        List<Integer> integers = edges.get(edgeIndex);
+        if(integers.get(0) == parentIndex){
+            return integers.get(1);
+        }else {
+            return integers.get(0);
+        }
     }
 
-    private int sum(int parentEdgeIndex, int currentIndex) {
+    private void sum(int parentEdgeIndex, int currentIndex) {
         int childrenWeights = 0;
         for (int edgeIndex = 0; edgeIndex < inсidence[0].length; edgeIndex++) {
             if (inсidence[currentIndex][edgeIndex] && edgeIndex != parentEdgeIndex) {
                 int childIndex = getChildIndex(edgeIndex, currentIndex);
-                childrenWeights += sum(edgeIndex, childIndex);
+                sum(edgeIndex, childIndex);
+                Integer subtreeWeight = subtreesWeights[edgeIndex][0];
+                subtreesWeights[edgeIndex][1] = totalWeight - subtreeWeight;
+
+
+                childrenWeights += subtreeWeight;
             }
         }
 
-        return childrenWeights + weights[currentIndex];
+        if (subtreesWeights[parentEdgeIndex] != null) {
+            throw new IllegalStateException("Edge " + parentEdgeIndex + " has already been modified");
+        } else {
+            subtreesWeights[parentEdgeIndex] =
+                    new int[]{childrenWeights + weights[currentIndex], 0};
+        }
     }
 
+
+    private int smallestDiff() {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < subtreesWeights.length; i++) {
+            int currentDiff = Math.abs(subtreesWeights[i][0] - subtreesWeights[i][1]);
+            if (currentDiff < min) {
+                min = currentDiff;
+            }
+        }
+        return min;
+    }
 }
 
 public class CutTheTree {
     public static void main(String[] args) throws IOException {
-        /*BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("ctt.in"));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int n = Integer.parseInt(bufferedReader.readLine().trim());
@@ -105,27 +134,26 @@ public class CutTheTree {
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
-        });*/
-
-        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        });
+        /*List<Integer> data = Arrays.asList(1, 2, 4, 4, 5, 6, 7, 9);
         List<List<Integer>> edges = Arrays.asList(
-                Arrays.asList(6, 7),
-                Arrays.asList(0, 6),
-                Arrays.asList(0, 3),
-                Arrays.asList(3, 5),
-                Arrays.asList(3, 4),
-                Arrays.asList(0, 1),
-                Arrays.asList(1, 2)
-        );
+                Arrays.asList(7, 8),
+                Arrays.asList(1, 7),
+                Arrays.asList(1, 4),
+                Arrays.asList(4, 6),
+                Arrays.asList(4, 5),
+                Arrays.asList(1, 2),
+                Arrays.asList(2, 3)
+        );*/
 
         TreeCutter treeCutter = new TreeCutter(data, edges);
         int diff = treeCutter.cutTheTree();
-        System.out.println(diff);
+        /*System.out.println(diff);*/
 
-        /*bufferedWriter.write(String.valueOf(treeCutter));
+        bufferedWriter.write(String.valueOf(diff));
         bufferedWriter.newLine();
 
         bufferedReader.close();
-        bufferedWriter.close();*/
+        bufferedWriter.close();
     }
 }
